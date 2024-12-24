cba0ffb60e5855001ffbf35a6154351ddf33e0f5
pikalov-a <apikalov1979@gmail.com>
2024-12-24 11:38:35 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,8 +1,8 @@
-# Генераторные сборки#module_9_3.py
+# Генераторные сборки#module_9_2.py
 ##Задача "":
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 first_result= (len(x)-len(y) for x,y in zip(first,second) if len(x) != len(y))
 print(list(first_result))
-second_result= (len(x) == len(y) for x in first for y in second if x != y )
+second_result= (len(first[i]) == len(second[j]) for i in range(len(first)) for j in range(len(second)) if i==j )
 print(list(second_result))
